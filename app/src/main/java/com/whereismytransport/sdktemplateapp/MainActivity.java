9de4b11441126a7d3e7124e0222ecc4f6aa9996f
Arne <arne@whereismytransport.com>
2018-09-27 10:39:54 +0200

    Added project source.

---
+++ app/src/main/java/com/whereismytransport/sdktemplateapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.whereismytransport.sdktemplateapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.whereismytransport.sdktemplateapp.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
